7dcdfc0bf77f24783060512ba89f38b86de94c00
Bogdan Hadadea <bogdan.hadadea@gmail.com>
2016-06-07 16:54:38 +0300

    initial commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ReactIntegrationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        die('here');
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# react-integration-bundle
+A Symfony Bundle for integrating ReactJS application within the Symfony project.
